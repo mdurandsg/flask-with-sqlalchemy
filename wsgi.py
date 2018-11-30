52bf9c4462d1bdd0797016d62db9fbef7148f862
Michel Durand <michel.durand@socgen.com>
2018-11-30 14:37:38 +0100

    Add admin page

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, render_template
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -10,6 +14,9 @@ ma = Marshmallow(app)
 
 from models import Product
 from schemas import products_schema, product_schema
+
+admin = Admin(app, name="Back-office", template_mode="bootstrap3")
+admin.add_view(ModelView(Product, db.session))
 
 @app.route('/')
 def hello():
